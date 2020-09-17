be531c00fcad47925546c0db8c4cf09b5085e541
Olusanmi Hundogan <o.hundogan@gmail.com>
2020-09-17 17:32:48 +0200

    Impl: cell area computation

--- reader.py
+++ reader.py
@@ -31,7 +31,6 @@ class DataSet:
     def read(self):
         raise NotImplementedError
 
-
     def load_files_in_memory(self):
         assert self.has_descriptors, f"Dunno the file locations. Run {self.read.__name__} function first."
         self.full_data = [{"meta_data": file, "data": self._load_ply(file["path"]) if not file["type"] == ".off" else self._load_off(file["path"])} for file in self.data_descriptors]
@@ -59,36 +58,54 @@ class DataSet:
 
     def detect_outliers(self):
         assert self.has_stats, f"No statistics were computed. Run {self.compute_shape_statistics.__name__} first"
+
         def add_to_stats(m_object):
             m_object["statistics"]["faces_outlier"] = m_object["faces_outlier"]
             m_object["statistics"]["vertices_outlier"] = m_object["vertices_outlier"]
             return m_object
-        
+
         all_faces_counts, all_vertices_counts = self.all_statistics["faces"], self.all_statistics["vertices"]
         self.faces_mean, self.faces_std = all_faces_counts.mean(), all_faces_counts.std()
         self.vertices_mean, self.vertices_std = all_faces_counts.mean(), all_faces_counts.std()
         init_pipe = (mesh_object for mesh_object in self.full_data)
-        faces_pipe = (dict(**mesh_object, faces_outlier = True if np.abs(mesh_object["statistics"]["faces"] - self.faces_mean) > 2 * self.faces_std else False) for mesh_object in init_pipe)
-        vertices_pipe = (dict(**mesh_object, vertices_outlier = True if np.abs(mesh_object["statistics"]["vertices"] - self.vertices_mean) > 2 * self.vertices_std else False) for mesh_object in faces_pipe)
+        faces_pipe = (dict(**mesh_object, faces_outlier=True if np.abs(mesh_object["statistics"]["faces"] - self.faces_mean) > 2 * self.faces_std else False) for mesh_object in init_pipe)
+        vertices_pipe = (dict(**mesh_object, vertices_outlier=True if np.abs(mesh_object["statistics"]["vertices"] - self.vertices_mean) > 2 * self.vertices_std else False)
+                         for mesh_object in faces_pipe)
         add_to_stats_pipe = (add_to_stats(mesh_object) for mesh_object in vertices_pipe)
 
         self.full_data = list(add_to_stats_pipe)
         self.all_statistics = pd.DataFrame([mesh_object["statistics"] for mesh_object in self.full_data])
         print(f"Finished {inspect.currentframe().f_code.co_name}")
 
-
     def _compute_statistics(self, mesh):
         mesh_data = mesh["data"]
         poly_data_object = mesh["poly_data"]
+        triangulized_poly_data_object = poly_data_object.triangulate()
+        mesh["poly_data"] = triangulized_poly_data_object
         statistics = {}
         statistics["label"] = mesh["meta_data"]["label"]
         statistics["faces"] = poly_data_object.n_faces
         statistics["vertices"] = poly_data_object.n_points
         statistics.update(dict(zip(["bound_" + b for b in "xmin xmax ymin ymax zmin zmax".split()], poly_data_object.bounds)))
-        # TODO: Fix pandas to csv issue with difference attribute numbers
-        cell_counter = Counter([len(cell) for cell in self._get_cells(poly_data_object)])
-        statistics.update({f"cell_type_{k}":v for k,v in cell_counter.items()})
+        cell_ids = self._get_cells(poly_data_object)
+        cell_point_counts = [len(cell) for cell in cell_ids]
+        cell_counter = Counter(cell_point_counts)
+        statistics.update({f"cell_type_{k}": v for k, v in cell_counter.items()})
+
+        statistics.update({f"bary_center_{coord}": val for val, coord in zip(triangulized_poly_data_object.center, "x,y,z".split(","))})
+        cell_areas = self._get_cell_areas(triangulized_poly_data_object.points, cell_ids, cell_point_counts)
+        
+        statistics["cell_area_mean"] = np.mean(cell_areas)
+        statistics["cell_area_std"] = np.std(cell_areas)
         return statistics
+
+    def _get_cell_areas(self, mesh_vertices, mesh_cells, mesh_cell_point_counts):
+        cell_combinations = mesh_vertices[mesh_cells, :]
+        # cell_areas = [np.abs(np.linalg.norm(np.cross((matrix[0] - matrix[1]), (matrix[0] - matrix[2])))) / 2 for matrix in cell_combinations]
+        cell_areas_fast = np.abs(np.linalg.norm(np.cross((cell_combinations[:,0] - cell_combinations[:, 1]), (cell_combinations[:,0] - cell_combinations[:,2])), axis=1))/2 # https://math.stackexchange.com/a/128999
+        
+        return cell_areas_fast
+
 
     def _get_cells(self, mesh):
         """Returns a list of the cells from this mesh.
@@ -101,7 +118,7 @@ class DataSet:
             loc = i + offset
             nc = mesh.faces[loc]
             offset += nc
-            cell = mesh.faces[loc+1:loc+nc+1]
+            cell = mesh.faces[loc + 1:loc + nc + 1]
             cells.append(cell)
         return cells
 
@@ -133,9 +150,9 @@ class DataSet:
 
 class PSBDataset(DataSet):
     def __init__(self, search_paths=None, stats_path=None):
-        self.search_paths = [Path("data/psb") / "**/*.aff", Path("data/psb") / "**/*.ply"] if not search_paths else search_paths
+        self.search_paths = [Path("data/psb") / "**/*.off", Path("data/psb") / "**/*.ply"] if not search_paths else search_paths
         assert type(self.search_paths) == list, f"Provide a list for the search paths not a {type(self.search_paths)}"
-        self.search_paths = [Path(str(myString)) for myString in self.search_paths] #if not self.search_paths else self.search_paths
+        self.search_paths = [Path(str(myString)) for myString in self.search_paths]  #if not self.search_paths else self.search_paths
         # assert self.search_paths, "No search paths given"
         stats_path = Path("data/psb") if not stats_path else stats_path
         super().__init__(self.search_paths, self.stats_path)
