923bcd51735849923218fe8c7adf543d8c25185c
Filippo M. Libardi <pippo.libardi@gmail.com>
2020-09-24 17:04:42 +0200

    Fixed indexing mismatching

--- normalizer.py
+++ normalizer.py
@@ -9,10 +9,9 @@ class Normalizer:
 
     def __init__(self):
         self.num_avg_verts = 35000
-
         # self.reader = PSBDataset(search_path="D:\\Documents\\Programming\\Python\\project_multimedia_retrieval\\test_mesh")
-        ## self.reader = PSBDataset(search_path="D:\\Downloads\\psb_v1\\benchmark\\db")
-        self.reader = PSBDataset(search_path="C:\\Users\\chris\\OneDrive\\Dokumente\\Utrecht Uni Docs\\5.Period\\MS\\psb_v1\\benchmark\\db")
+        self.reader = PSBDataset(search_path="D:\\Downloads\\psb_v1\\benchmark\\db")
+        # self.reader = PSBDataset(search_path="C:\\Users\\chris\\OneDrive\\Dokumente\\Utrecht Uni Docs\\5.Period\\MS\\psb_v1\\benchmark\\db")
         self.reader.read()
         self.reader.load_files_in_memory()
         self.reader.convert_all_to_polydata()
@@ -22,32 +21,22 @@ class Normalizer:
         self.history.append([item["poly_data"] for item in self.full_data])
         # print(self.history[-1][0].bounds)
 
-    #https://www.grasshopper3d.com/forum/topics/best-uniform-remesher-for-patterning-organic-suraces
+    # https://www.grasshopper3d.com/forum/topics/best-uniform-remesher-for-patterning-organic-suraces
     def uniform_remeshing(self):
         tmp_mesh = []
-        remesh = None
+        print("Remeshing")
         for mesh in self.history[-1]:
             data = mesh.clean()
             clus = pyacvd.Clustering(data)
-            # if len(mesh.points) < 3500:
             while len(clus.mesh.points) < 10000:
-                clus.subdivide(1)
+                clus.subdivide(2)
             clus.cluster(10000)
             remesh = clus.create_mesh()
             tmp_mesh.append(remesh)
         self.history.append(tmp_mesh)
 
-            # if mesh.n_faces < 9000:
-            #     clust_mesh = pyacvd.Clustering(data)
-            #     clust_mesh.subdivide(1)
-            #     clust_mesh.cluster(10000)
-            #     remesh = PolyData(clust_mesh.mesh.points, clust_mesh.mesh.faces)
-            # # elif len(mesh.points) > self.num_avg_verts:
-            # if mesh.n_faces > 11000:
-            #     remesh = data.decimate(10000/mesh.n_faces)
-
-
     def center(self):
+        print("Centering")
         tmp_mesh = []
         for mesh, full_mesh_stuff in zip(self.history[-1], self.full_data):
             remesh = PolyData(mesh.points.copy(), mesh.faces.copy())
@@ -55,9 +44,9 @@ class Normalizer:
             remesh.translate(np.zeros_like(offset) - offset)
             tmp_mesh.append(remesh)
         self.history.append(tmp_mesh)
-        # print(self.history[-1][0].bounds)
 
     def align(self):
+        print("Aligning")
         tmp_mesh = []
         for mesh in self.history[-1]:
             A_cov = np.cov(mesh.points.T)
@@ -70,7 +59,7 @@ class Normalizer:
         self.history.append(tmp_mesh)
 
     def scale_to_union(self):
-        # print(self.history[-1][0].bounds)
+        print("Scaling")
         tmp_mesh = []
         for mesh in self.history[-1]:
             max_range = np.max(mesh.points, axis=0)
@@ -81,17 +70,16 @@ class Normalizer:
             remesh = PolyData(scaled_points, mesh.faces.copy())
             tmp_mesh.append(remesh)
         self.history.append(tmp_mesh)
-        # print(self.history[-1][0].bounds)
 
     def save_dataset(self):
         c_idx = 0
-        for index, mesh in enumerate(self.history[-1]):
+        for processed_mesh, data in zip(self.history[-1], self.full_data):
             # if (index % 100 == 0) and (index != 0):
             #     c_idx += 1
-            # print(f"Writing m{index}.ply to data\\{c_idx}\\")
-            # mesh.save(f"data\\{c_idx}\\m{index}.ply")
-            print(f"Writing m{index}.ply to data")
-            mesh.save(f"data\\m{index}.ply")
+            print(f"Writing {data['meta_data']['name']}.ply to data\\{data['meta_data']['label']}\\")
+            processed_mesh.save(f"data\\{data['meta_data']['label']}\\m{data['meta_data']['name']}.ply")
+            # print(f"Writing m{index}.ply to data")
+            # mesh.save(f"data\\m{index}.ply")
 
 
 if __name__ == '__main__':
