4b519090ff45264d772750d20b905a218f8c3c02
Christian Moll <christian.moll@live.de>
2020-09-26 19:16:09 +0200

    Create subforlder added in normaliyer save to file

--- trash/bar.py
+++ trash/bar.py
@@ -26,23 +26,35 @@ readerOrig.compute_shape_statistics()
 stats = readerOrig.all_statistics
 cell_std_orig = stats.cell_area_std
 # print(stats.cell_area_mean)
-plt.hist(rescale(cell_std_orig), bins=20)
+plt.hist(rescale(cell_std_orig), bins=20, label="original")
 
-
+#
 readerNorm = PSBDataset(
-    search_path="data")
+    search_path="dataAlmostNorm")
 readerNorm.read()
 readerNorm.load_files_in_memory()
 readerNorm.convert_all_to_polydata()
 readerNorm.compute_shape_statistics()
 stats = readerNorm.all_statistics
 cell_std_norm = stats.cell_area_std
-plt.hist(rescale(cell_std_norm), bins=20, alpha=.5)
-# print(stats.cell_area_mean)
+plt.hist(rescale(cell_std_norm), bins=20, alpha=.5, label="remeshed")
+print(stats.cell_area_mean)
 
 
 
 # from matplotlib.ticker import PercentFormatter
 # plt.gca().xaxis.set_major_formatter(PercentFormatter(1))
 
+plt.legend()
+plt.xlabel('Standard Deviations of Meshes')
+plt.ylabel('Nr. of Meshes')
 plt.show()
+print("min orig: ", np.min(cell_std_orig))
+print("max orig: ", np.max(cell_std_orig))
+print("avg orig: ", np.mean(cell_std_orig))
+print("std orig: ", np.std(cell_std_orig))
+print("min remesh: ", np.min(cell_std_norm))
+print("max remesh: ", np.max(cell_std_norm))
+print("avg remesh: ", np.mean(cell_std_norm))
+print("std remesh: ", np.std(cell_std_norm))
+print("bla")
