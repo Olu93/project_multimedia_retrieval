3104031c4101b5e7b1065d8ade45f6c17d4c22ea
Olusanmi Hundogan <o.hundogan@gmail.com>
2020-10-05 15:26:53 +0200

    Impl: base for histograsm visualisation

--- visualisation.py
+++ visualisation.py
@@ -4,19 +4,51 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 import matplotlib.tri as mtri
+from reader import DataSet
 
 #%%
 FE = FeatureExtractor()
 
-# %%
-example1 = FE.full_data[0]
-example2 = FE.full_data[3]
-result1 = list(FE.cube_root_volume_four_rand_verts(example1).values())[0]
-result2 = list(FE.cube_root_volume_four_rand_verts(example2).values())[0]
-ex_points1 = example1["poly_data"].points
-ex_points2 = example2["poly_data"].points
-# %%
 
+# %%
+def plot_mesh(mesh, ax):
+    points = mesh.points
+    X, Y, Z = points[:, 0], points[:, 1], points[:, 2]
+    faces = DataSet._get_cells(mesh)
+    return ax.plot_trisurf(X, Y, Z=Z, triangles=faces)
+
+
+def visualize_histogram(extractor, feature_extraction_function, item_ids=[0, 1]):
+    result_sets = [(data, list(feature_extraction_function(data).values())[0]) for data in np.array(extractor.full_data)[item_ids]]
+    num_items = len(result_sets)
+    fig = plt.figure(figsize=(5 * num_items, 8))
+    axes = [(fig.add_subplot(2, num_items, idx + 1), fig.add_subplot(2, num_items, num_items + idx + 1, projection='3d')) for idx in range(num_items)]
+    for (hist_ax, mesh_ax), (data, results) in zip(axes, result_sets):
+        hist_ax.plot(results)
+        plot_mesh(data["poly_data"], mesh_ax)
+    fig.tight_layout()
+    return fig.show()
+
+
+visualize_histogram(FE, FE.cube_root_volume_four_rand_verts, list(range(4)))
+# example1 = FE.full_data[0]
+# example2 = FE.full_data[1]
+# result1 = list(FE.cube_root_volume_four_rand_verts(example1).values())[0]
+# result2 = list(FE.cube_root_volume_four_rand_verts(example2).values())[0]
+# axes = fig.add_subplot(221), fig.add_subplot(222), fig.add_subplot(223, projection='3d'), fig.add_subplot(224, projection='3d')
+# fig, axes = plt.subplots(2, 2, subplot_kw=dict(projection='3d'))
+# axes[0].plot(result1)
+# axes[1].plot(result2)
+# plot_mesh(example1["poly_data"], axes[2])
+# plot_mesh(example2["poly_data"], axes[3])
+# %%
+# fig, axes = plt.subplots(1, 2, subplot_kw=dict(projection='3d'))
+fig = plt.figure()
+fig.add_subplot(2, 2, 1)
+fig.add_subplot(2, 2, 3)
+plt.tight_layout()
+
+# %%
 
 subsample_factor = 10
 X, Y, Z = np.meshgrid(ex_points1[::subsample_factor, 0], ex_points1[::subsample_factor, 1], ex_points1[::subsample_factor, 2])
@@ -31,16 +63,7 @@ foo = pv.PolyData(points)
 
 # %%
 
-
 # %%
-from reader import DataSet
-
-subsample_factor = 1
-X, Y, Z = ex_points1[::subsample_factor, 0], ex_points1[::subsample_factor, 1], ex_points1[::subsample_factor, 2]
-faces = DataSet._get_cells(example1["poly_data"])
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-ax.plot_trisurf(X, Y, Z=Z, triangles=faces)
 
 # %%
 import pyvista as pv
@@ -69,6 +92,6 @@ z_ = np.linspace(3., 4., 10000)[::100]
 
 x, y, z = np.meshgrid(x_, y_, z_, indexing='ij')
 
-assert np.all(x[:,0,0] == x_)
-assert np.all(y[0,:,0] == y_)
-assert np.all(z[0,0,:] == z_)
+assert np.all(x[:, 0, 0] == x_)
+assert np.all(y[0, :, 0] == y_)
+assert np.all(z[0, 0, :] == z_)
