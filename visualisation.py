e5e78525429dbcf0933607f60a32ced8d80c3675
Olusanmi Hundogan <o.hundogan@gmail.com>
2020-10-05 17:00:23 +0200

    Visualize everything

--- visualisation.py
+++ visualisation.py
@@ -7,5 +7,6 @@ plot_names = "Ant Human Guitar1 Guitar2".split()
 # %%
 visualize_histogram(FE, "cube_root_volume_four_rand_verts", list(range(4)), plot_names)
 visualize_histogram(FE, "angle_three_rand_verts", list(range(4)), plot_names)
-
+visualize_histogram(FE, "dist_two_rand_verts", list(range(4)), plot_names)
+visualize_histogram(FE, "dist_bar_vert", list(range(4)), plot_names)
 # %%
