4768e32adcd11eeea16b7d881421eab4273df8b7
Olusanmi Hundogan <o.hundogan@gmail.com>
2020-10-05 16:46:16 +0200

    Impr: Added automatic title inference to function

--- visualisation.py
+++ visualisation.py
@@ -16,6 +16,7 @@ def plot_mesh(mesh, ax):
 def visualize_histogram(extractor, function_name, item_ids=[0, 1], names=None):
     feature_extraction_function = getattr(extractor, function_name)
     names = names if names else [data["meta_data"]["label"] for data in np.array(extractor.full_data)[item_ids]]
+    feature_name = list(feature_extraction_function(extractor.full_data[0]).keys())[0]
     result_sets = [(data, list(feature_extraction_function(data).values())[0]) for data in np.array(extractor.full_data)[item_ids]]
     num_items = len(result_sets)
     num_bins = extractor.number_bins
@@ -25,12 +26,12 @@ def visualize_histogram(extractor, function_name, item_ids=[0, 1], names=None):
         hist_ax.set_title(name)
         hist_ax.bar(np.linspace(0, 1, num_bins), results, 1 / num_bins, align='center')
         plot_mesh(data["poly_data"], mesh_ax)
-    # fig.suptitle(result_sets[0][0], fontsize=16)
+    fig.suptitle(feature_name, fontsize=20)
     fig.tight_layout()
     return fig.show()
-# #%%
-# FE = FeatureExtractor()
-# plot_names = "Ant Human Guitar1 Guitar2".split()
+#%%
+FE = FeatureExtractor()
+plot_names = "Ant Human Guitar1 Guitar2".split()
 # %%
 visualize_histogram(FE, "cube_root_volume_four_rand_verts", list(range(4)), plot_names)
 visualize_histogram(FE, "angle_three_rand_verts", list(range(4)), plot_names)
