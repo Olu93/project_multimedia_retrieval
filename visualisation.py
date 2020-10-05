5799331643e2eac69b1e8550d54a33a3f3f96307
Olusanmi Hundogan <o.hundogan@gmail.com>
2020-10-05 15:58:45 +0200

    Impl: visualisation for histograms function

--- visualisation.py
+++ visualisation.py
@@ -8,6 +8,13 @@ from reader import DataSet
 
 #%%
 FE = FeatureExtractor()
+# %%
+example1 = FE.full_data[0]
+# example2 = FE.full_data[1]
+result1 = list(FE.cube_root_volume_four_rand_verts(example1).values())[0]
+# plt.hist(result1, bins=np.linspace(0, 1, 10))
+plt.bar(np.linspace(0, 1, 10), result1, .1, align='center')
+result1
 
 
 # %%
@@ -21,19 +28,19 @@ def plot_mesh(mesh, ax):
 def visualize_histogram(extractor, feature_extraction_function, item_ids=[0, 1]):
     result_sets = [(data, list(feature_extraction_function(data).values())[0]) for data in np.array(extractor.full_data)[item_ids]]
     num_items = len(result_sets)
+    num_bins = FE.number_bins
     fig = plt.figure(figsize=(5 * num_items, 8))
     axes = [(fig.add_subplot(2, num_items, idx + 1), fig.add_subplot(2, num_items, num_items + idx + 1, projection='3d')) for idx in range(num_items)]
     for (hist_ax, mesh_ax), (data, results) in zip(axes, result_sets):
-        hist_ax.plot(results)
+        hist_ax.bar(np.linspace(0, 1, num_bins), results, 1 / num_bins, align='center')
         plot_mesh(data["poly_data"], mesh_ax)
     fig.tight_layout()
     return fig.show()
 
 
 visualize_histogram(FE, FE.cube_root_volume_four_rand_verts, list(range(4)))
-# example1 = FE.full_data[0]
-# example2 = FE.full_data[1]
-# result1 = list(FE.cube_root_volume_four_rand_verts(example1).values())[0]
+# %%
+
 # result2 = list(FE.cube_root_volume_four_rand_verts(example2).values())[0]
 # axes = fig.add_subplot(221), fig.add_subplot(222), fig.add_subplot(223, projection='3d'), fig.add_subplot(224, projection='3d')
 # fig, axes = plt.subplots(2, 2, subplot_kw=dict(projection='3d'))
